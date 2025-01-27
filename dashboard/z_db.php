19e07c74247de6f53dcc9aa9a3aeb1b89a6c372e
LawinMagozore <magozorel@gmail.com>
2025-01-27 05:25:52 +0200

    update

--- dashboard/z_db.php
+++ dashboard/z_db.php
@@ -1,19 +1,18 @@
 <?php
-$con = new mysqli("localhost", "root", "", "vogue");
+$con = new mysqli("localhost:3306", "ledhame1_ledhame1", "Masvingo123", "ledhame1_vogue");
 if ($con->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
 //Your Website URL Goes Here
-$url="http://localhost/vogue";
+$url = "http://localhost/vogue";
 
 
 //Set Blog Activation Bonus Here (It must be only Number)
-$blog_bonus ="10";
+$blog_bonus = "10";
 //Set Article Activation Bonus Here (It must be only Number)
-$art_bonus="10";
+$art_bonus = "10";
 //Set Daily Login Bonus Here (It must be only Number)
-$login_bonus="10";
+$login_bonus = "10";
 //Set Currency Symbol for daily login bonus Here
-$money="$";
-?>
+$money = "$";
