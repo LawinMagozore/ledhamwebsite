cf1e68f8d84a64b2ce6977a861d0bcb1faef0fb9
LawinMagozore <magozorel@gmail.com>
2025-01-27 05:19:39 +0200

    update

--- z_db.php
+++ z_db.php
@@ -2,9 +2,6 @@
 $con = new mysqli("localhost:3306", "ledhame1_ledhame1", "Masvingo123", "ledhame1_vogue");
 if ($con->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-} else {
-
-    echo "database connected succesfully";
 }
 
 //Your Website URL Goes Here
